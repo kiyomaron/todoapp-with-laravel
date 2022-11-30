1fd711dd281ef1a9b6a12c415b5fc343d7e67b70
kiyomaron <kyn.maron947@gmail.com>
2022-11-30 21:19:04 +0900

    Add update functions

--- resources/ts/pages/tasks/components/TaskItem.tsx
+++ resources/ts/pages/tasks/components/TaskItem.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { Task } from "../../../types/Task"
-import { useUpdateDoneTask } from "../../../queries/TaskQuery"
+import { useUpdateDoneTask, useUpdateTask, useDeleteTask } from "../../../queries/TaskQuery"
+import { toast } from "react-toastify"
+import { updateTask } from "../../../api/TaskAPI"
 
 type Props = {
     task: Task
@@ -8,6 +10,75 @@ type Props = {
 
 const TaskItem: React.FC<Props> = ({ task }) => {
     const updateDoneTask = useUpdateDoneTask()
+    const updateTask = useUpdateTask()
+
+    const [editTitle, setEditTitle] = useState<string|undefined>(undefined)
+
+
+    const handleInputChange = (e: React.KeyboardEvent<HTMLInputElement> | React.ChangeEvent<HTMLInputElement>) => {
+        setEditTitle(e.currentTarget.value)
+    }
+
+    const handleUpdate = (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+
+        if(!editTitle) {
+            toast.error('タイトルを入力してください。')
+            return
+        }
+            const newTask = {...task}
+            newTask.title = editTitle
+
+            updateTask.mutate({
+                id: task.id,
+                task: newTask
+            })
+
+            setEditTitle(undefined)
+        
+    }
+
+    const handleToggleEdit = () => {
+        setEditTitle(task.title)
+    }
+
+    const handleOnKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(['Escape', 'Tab'].includes(e.key)) {
+            setEditTitle(undefined)
+        }
+    }
+
+
+    const itemInput = () => {
+        return (
+            <>
+                <form onSubmit={handleUpdate}>
+                    <input
+                        type="text"
+                        className="input"
+                        defaultValue={editTitle}
+                        onChange={handleInputChange}
+                        onKeyDown={handleOnKey}
+                    />
+                </form>
+                <button className="btn" onClick={handleUpdate}>更新</button>
+            </>
+        )
+    }
+
+    const itemText = () => {
+        return (
+            <>
+                <div onClick={handleToggleEdit}>
+                    <span>{task.title}</span>
+                </div>
+                <button className="btn is-delete">
+                    削除
+                </button>
+            </>
+        )
+    }
+
     return (
         <li className={task.is_done ? 'done' : ''}>
         <label className="checkbox-label">
@@ -17,8 +88,7 @@ const TaskItem: React.FC<Props> = ({ task }) => {
                 onClick={() => updateDoneTask.mutate(task)}
             />
         </label>
-        <div><span>{task.title}</span></div>
-        <button className="btn is-delete">削除</button>
+        { editTitle === undefined ? itemText() : itemInput() }
     </li>
     ) 
 }
--- resources/ts/queries/TaskQuery.ts
+++ resources/ts/queries/TaskQuery.ts
@@ -44,8 +44,49 @@ const useCreateTask = () => {
                     }
                 )
             } else {
+                toast.error('登録に失敗しました。')
+            }
+        }
+    })
+}
+
+const useUpdateTask = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation(api.updateTask, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('tasks')
+            toast.success('更新に成功しました。')
+        },
+        onError: (error: AxiosError<MyErrorResponse>) => {
+        // console.log(error.response?.data);
+            
+            if (error.response?.data.errors) {
+                Object.values(error.response?.data.errors).map(
+                    (messages: any) => {
+                        messages.map((message: string) => {
+                            toast.error(message)
+                        })
+                    }
+                )
+            } else {
                 toast.error('更新に失敗しました。')
             }
+        }
+    })
+}
+
+const useDeleteTask = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation(api.deleteTask, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('tasks')
+            toast.success('削除に成功しました。')
+        },
+        onError: () => {
+
+            toast.error('削除に失敗しました。')
         }
     })
 }
@@ -53,5 +94,7 @@ const useCreateTask = () => {
 export {
     useTasks,
     useUpdateDoneTask,
-    useCreateTask
+    useCreateTask,
+    useUpdateTask,
+    useDeleteTask
 }
