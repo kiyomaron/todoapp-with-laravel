6242f0d524ede21af16cb41e844173ae2edd03ee
kiyomaron <kyn.maron947@gmail.com>
2022-12-02 08:33:05 +0900

    Get user info from login page

---
+++ resources/ts/queries/AuthQuery.ts
@@ -0,0 +1,35 @@
+import * as api from "../api/AuthAPI"
+import { useQuery, useMutation } from "react-query"
+import { toast } from "react-toastify"
+const useUser = () => {
+    return useQuery('users', () => api.getUser())
+} 
+
+const useLogin = () => {
+    return useMutation(api.login, {
+        onSuccess: (user) => {
+            console.log(user);
+        },
+        onError: () => {
+            toast.error('ログインに失敗しました。')
+        }
+    })
+}
+
+const useLogout = () => {
+    return useMutation(api.logout, {
+        onSuccess: (user) => {
+            console.log(user);
+        },
+        onError: () => {
+            toast.error('ログアウトに失敗しました。')   
+        }
+    })
+}
+
+
+export {
+    useUser,
+    useLogin,
+    useLogout
+}
